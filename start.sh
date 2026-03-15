c815852141e3faf2d32027ee3f3a08672bc2958c
Aaditya Prabhu <aaditya.prabhu@gmail.com>
2026-03-15 12:38:33 +0530

    Fix Gotenberg binding port issue on startup

--- start.sh
+++ start.sh
@@ -5,5 +5,5 @@ echo "Starting Caddy on port 8080..."
 caddy run --config /etc/caddy/Caddyfile &
 
 # Start Gotenberg in the foreground using its standard binary
-echo "Starting Gotenberg..."
-exec gotenberg "$@"
+echo "Starting Gotenberg Engine..."
+exec gotenberg --api-port=3000 "$@"
